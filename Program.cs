4c1caeaf2264265f6d235ce9ce00cfd009747664
Ryan Lima <138833109+Ryan-Felix-Lima@users.noreply.github.com>
2023-11-10 16:58:12 -0300

    Update Program.cs 2.0

--- Program.cs
+++ Program.cs
@@ -1,3 +1,39 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+using System;
+
+namespace DesafioPOO.Tests
+{
+    class Program
+    {
+        static void Main()
+        {
+            TestarNokia();
+            TestarIphone();
+        }
+
+        static void TestarNokia()
+        {
+            Nokia telefoneNokia = new Nokia();
+
+            // Realizar testes específicos para o Nokia
+            Console.WriteLine("Testando Nokia...");
+            // Adicione sua lógica de teste aqui
+
+            Console.WriteLine("Testes do Nokia concluídos.");
+            Console.WriteLine();
+        }
+
+        static void TestarIphone()
+        {
+            Iphone iphone = new Iphone();
+
+            // Realizar testes específicos para o Iphone
+            Console.WriteLine("Testando Iphone...");
+            // Adicione sua lógica de teste aqui
+
+            Console.WriteLine("Testes do Iphone concluídos.");
+            Console.WriteLine();
+        }
+    }
+}
